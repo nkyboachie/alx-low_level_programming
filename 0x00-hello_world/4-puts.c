103874740f050ace68322b14d57601866c8e82ff
nkyboachie <nkyboachie@gmail.com>
2022-12-08 12:00:51 +0000

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Description: ' prints a string  using puts()'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char str[60];
+
+	strpro(str, "Programming is like building a multilingual puzzle");
+
+	puts(strpro);
+return (0);
+}
+
