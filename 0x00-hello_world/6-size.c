a5af2a323e85749d864d06c65c5b4c1d244ab690
nkyboachie <nkyboachie@gmail.com>
2022-12-08 13:29:55 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,11 +19,11 @@ long long int any_longlong;
 float any_float;
 
 
-printf("Size of a char: %zu bytes(s)", sizeof(any_char));
-printf("Size of an int: %zu bytes(s)", sizeof(any_int));
-printf("Size of a long int: %zu bytes(s)", sizeof(any_long));
-printf("Size of a long long int: %zu bytes(s)", sizeof(any_longlong));
-printf("Size of a float: %zu byte(s)", sizeof(any_float));
+printf("Size of a char: %zu bytes(s)\n", sizeof(any_char));
+printf("Size of an int: %zu bytes(s)\n", sizeof(any_int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(any_long));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(any_longlong));
+printf("Size of a float: %zu byte(s)\n", sizeof(any_float));
 
 return (0);
 }
