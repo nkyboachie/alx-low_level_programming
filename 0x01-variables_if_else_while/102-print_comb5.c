78b9bab895ed304a3b1206a845938ff7d0c11f0d
nkyboachie <nkyboachie@gmail.com>
2022-12-09 19:49:28 +0000

    the end

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - combinations
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar('0' + i / 10);
+putchar('0' + i % 10);
+putchar(' ');
+putchar('0' + j / 10);
+putchar('0' + j % 10);
+if (i == 98 && j == 99)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
