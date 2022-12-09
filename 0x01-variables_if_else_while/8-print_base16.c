9ea86b128f8ba4b484c47227ea23ae56473a1bb6
nkyboachie <nkyboachie@gmail.com>
2022-12-09 14:45:03 +0000

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - print z-a with only putchar()
+*
+*Return: Always 0
+*/
+int main(void)
+{
+int ch;
+int hc;
+for (ch = 0; ch < 10; ch++)
+{
+putchar(ch+ '0');
+}
+for (hc = 97; hc < 102; hc++)
+{
+putchar(hc);
+}
+
+putchar('\n');
+return (0);
+}
