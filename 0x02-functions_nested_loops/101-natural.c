5dad31069875df27ed0d0b251e3a6f33d5304c0e
nkyboachie <nkyboachie@gmail.com>
2022-12-13 19:31:47 +0000

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,18 +2,18 @@
 
 /**
  * main - computes and prints the sum of all the multiples of 3 or 5 below 1024
- * return: 0
  *
+ * Return:Always 0.
  */
 int main(void)
 {
-int i = 0 , sum = 0;
+int i = 0, sum = 0;
 
 while (i < 1024)
 {
-if ((i%3) == 0 || (i%5) == 0)
+if ((i % 3) == 0 || (i % 5) == 0)
 
-sum +=i;
+sum += i;
 
 i++;
 }
