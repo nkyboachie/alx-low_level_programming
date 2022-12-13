85a09a5d72fc3f777f9bb0478a39dddcbda40ee5
nkyboachie <nkyboachie@gmail.com>
2022-12-13 20:55:57 +0000

    fibbb

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with
+ * main - Prints the first 98 Fibonacci numbers, starting with
  *        1 and 2, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
-int main() {
-  int a = 1;
-  int b = 2;
+int main(void)
+{
 	int i;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_h1, fib1_h2, fib2_h1, fib2_h2;
+	unsigned long h1, h2;
 
-  printf("%d, %d", a, b);
+	for (i = 0; i < 92; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
 
+		fib1 = fib2;
+		fib2 = sum;
+	}
 
-  for ( i = 0; i < 98; i++) {
-    int c = a + b;
-    printf(", %d", c);
-    a = b;
-    b = c;
-  }
+	fib1_h1 = fib1 / 10000000000;
+	fib2_h1 = fib2 / 10000000000;
+	fib1_h2 = fib1 % 10000000000;
+	fib2_h2 = fib2 % 10000000000;
 
+	for (i = 93; i < 99; i++)
+	{
+		h1 = fib1_h1 + fib2_h1;
+		h2 = fib1_h2 + fib2_h2;
+		if (fib1_h2 + fib2_h2 > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
 
-  printf("\n");
+		printf("%lu%lu", h1, h2);
+		if (i != 98)
+			printf(", ");
 
-  return 0;
+		fib1_h1 = fib2_h1;
+		fib1_h2 = fib2_h2;
+		fib2_h1 = h1;
+		fib2_h2 = h2;
+	}
+	printf("\n");
+	return (0);
 }
