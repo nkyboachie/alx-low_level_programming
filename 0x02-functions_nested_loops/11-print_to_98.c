3216d57bd2f1a5de9d00880b1d2f7719ba532df2
nkyboachie <nkyboachie@gmail.com>
2022-12-13 17:22:11 +0000

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@ void print_to_98(int n)
 {
     if (n >= 98)
     {
-        for (;n > 98;n--)
+        for (;n >= 98;n--)
             printf("%d, ",n--);
         printf("%d\n", n);
     }
 
     else
     {
-        for (;n < 98;n++)
+        for (;n <= 98;n++)
             printf("%d, ", n++);
         printf("%d\n", n);
     }
