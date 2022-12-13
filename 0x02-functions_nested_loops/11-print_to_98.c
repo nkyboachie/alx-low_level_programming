54c9f897cc4b5691a32ecb52c79ea8287f122062
nkyboachie <nkyboachie@gmail.com>
2022-12-13 17:28:23 +0000

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,16 @@
  */
 void print_to_98(int n)
 {
-    for (int i = n; i <= 98; i++)
-    {
-        putchar(i + '0');
+int i;
+for (i = n; i <= 98; i++)
+{
+putchar(i + '0');
+if (i != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
 
-        if (i != 98)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
+putchar('\n');
 }
