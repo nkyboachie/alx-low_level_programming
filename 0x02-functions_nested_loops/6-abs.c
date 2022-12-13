c0f3bbfc84db8beb8c12f4be7be86a8ec8660507
nkyboachie <nkyboachie@gmail.com>
2022-12-13 13:56:57 +0000

    6test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - absolute value of an integer
+ *@i: The integer to be computed.
+ *
+ * return absolute value
+ */
+int _abs(int i)
+{
+if (i >= 0)
+return (i);
+else
+return (-i);
+}
+
+
