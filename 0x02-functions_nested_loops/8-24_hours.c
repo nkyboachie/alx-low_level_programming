48802d56c693ab1e04ee09afe99ae8a9087d3c45
nkyboachie <nkyboachie@gmail.com>
2022-12-13 14:42:27 +0000

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 int hour, minute;
 hour = 0;
 minute = 0;
-while (hour <24)
+while (hour < 24)
 {
 while (minute < 60)
 {
-_putchar(hour/10 + '0');
+_putchar(hour / 10 + '0');
 _putchar(hour % 10 + '0');
 _putchar(':');
-_putchar(minute/10 + '0');
+_putchar(minute / 10 + '0');
 _putchar(minute % 10 + '0');
 _putchar('\n');
 minute++;
