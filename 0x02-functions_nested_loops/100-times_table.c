b7bf770a596f07ce47f9cdb7fa334b2f08a2aa51
nkyboachie <nkyboachie@gmail.com>
2022-12-13 18:47:09 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 void print_times_table(int n)
 {
 int num, mul, prod;
-if (n >= 0 && n <15)
+if (n >= 0 && n < 15)
 {
 for (num = 0; num <= n;  num++)
 {
@@ -29,7 +29,7 @@ if (prod >= 100)
 _putchar((prod / 100) + '0');
 _putchar(((prod / 10)) % 10 + '0');
 }
-else if (prod >= 10 && prod <=99)
+else if (prod >= 10 && prod <= 99)
 {
 _putchar((prod / 10) + '0');
 }
