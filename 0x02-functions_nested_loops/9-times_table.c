2db0ba5a7420db951307aaf62223f89d7ce098c8
nkyboachie <nkyboachie@gmail.com>
2022-12-13 16:13:52 +0000

    9test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,17 @@ void times_table(void)
 {
 int i, j, ij;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
 _putchar('0');
-for (j = 0; j < 10; j++)
+for (j = 0; j <= 9; j++)
 {
 _putchar(',');
 _putchar(' ');
 
 ij = i * j;
 
-if (ij < 10)
+if (ij <= 9)
 _putchar(' ');
 else
 _putchar((ij / 10) + '0');
