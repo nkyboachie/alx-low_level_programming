5cf605b291b5d98c026a21d2f9b2975796af8e24
nkyboachie <nkyboachie@gmail.com>
2022-12-15 14:27:28 +0000

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,17 +17,16 @@ char fb[] = "FizzBuzz";
 for (i = 1; i <= 100; i++)
 {
 if (i == 100)
-printf("%s ", f);
-else if (i % 5 == 0)
-printf("%s ", b);
-else if (i % 3 == 0)
 printf("%s ", b);
 else if ((i % 3 == 0) && (i % 5 == 0))
 printf("%s ", fb);
+else if (i % 5 == 0)
+printf("%s ", b);
+else if (i % 3 == 0)
+printf("%s ", f);
 else
 printf("%d ", i);
 }
 printf("\n");
 return (0);
 }
-
