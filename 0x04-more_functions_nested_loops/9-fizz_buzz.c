c5fc73b4c2e1ba2edc9d23649d15887196901b40
nkyboachie <nkyboachie@gmail.com>
2022-12-15 14:20:12 +0000

    tired

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,25 @@
  */
 int main(void)
 {
-int  n;
-char fb[] = "FizzBuzz";
+int i;
 char f[] = "Fizz";
 char b[] = "Buzz";
+char fb[] = "FizzBuzz";
 
-for (n = 1; n <= 100; n++)
+for (i = 1; i <= 100; i++)
 {
-if (n == 100)
-printf("%s ", b);
-else if (n % 3 == 0)
+if (i == 100)
 printf("%s ", f);
-else if (n % 5 == 0)
+else if (i % 5 == 0)
 printf("%s ", b);
-else if ((n % 3 == 0) && (n % 5 == 0))
+else if (i % 3 == 0)
+printf("%s ", b);
+else if ((i % 3 == 0) && (i % 5 == 0))
 printf("%s ", fb);
 else
-printf("%d ", n);
+printf("%d ", i);
 }
 printf("\n");
 return (0);
 }
+
