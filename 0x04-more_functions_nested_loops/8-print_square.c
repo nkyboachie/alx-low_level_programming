88c93aa694234c759a2bec4238dae2837b911cd7
nkyboachie <nkyboachie@gmail.com>
2022-12-15 14:04:22 +0000

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@
 void print_square(int size)
 {
 int i, j;
-if (size < 0)
+if (size <= 0)
 {
 _putchar('\n');
 }
@@ -18,6 +18,7 @@ else
 for (i = 1; i <= size; i++)
 {
 _putchar('#');
+
 for (j = 2; j <= size; j++)
 {
 _putchar('#');
