826764edae8169aea8b4617ccc977fd96e42251e
nkyboachie <nkyboachie@gmail.com>
2022-12-15 14:07:35 +0000

    tired

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print 1-100 multiples of 3 (Fizz)
+ * multiples of 5 (Buzz) muliples of 3&5 (FizzBuzz)
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int  n;
+char fb[] = "FizzBuzz";
+char f[] = "Fizz";
+char b[] = "Buzz";
+
+for (n = 1; n <= 100; n++)
+{
+if (n == 100)
+{
+printf("%s ", b);
+}
+else if (n % 3 == 0)
+{
+printf("%s ", f);
+}
+else if (n % 5 == 0)
+{
+printf("%s ", b);
+}
+else if (n % 3 == 0 && n % 5 == 0)
+{
+printf("%s ", fb);
+}
+else
+printf("%d ", n);
+}
+printf("\n");
+return (0);
+}
