a994491ad6eb310bc5847025cb23ab98dd48dee1
nkyboachie <nkyboachie@gmail.com>
2022-12-19 13:19:24 +0000

    Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of a & b
+ * @a: first value
+ * @b: second value
+ * Return: swapped values
+*/
+void swap_int(int *a, int *b)
+{
+    int temporary = *a;
+
+    *a = *b;
+    *b = temporary;
+
+}
