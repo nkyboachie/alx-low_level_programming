73b6fc47d40a8c48a1883341809dc5e54319d6ef
nkyboachie <nkyboachie@gmail.com>
2022-12-19 18:30:37 +0000

    done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,22 @@
-123abc
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(int argc, char **argv)
+{
+srand(time(NULL));
+
+char password[PASSWORD_LENGTH + 1];
+
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = rand() % 26 + 'a';
+}
+password[PASSWORD_LENGTH] = '\0';
+
+printf("Generated password: %s\n", password);
+
+return 0;
+}
